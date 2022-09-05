a8176d5e248691f6c1182e8d6aa7e87a7a6c1856
Aminu Rabiu <arabiu033@gmail.com>
2022-09-05 18:15:13 +0100

    completed the js file

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let div = document.querySelector("#normal-buttons");
 let menuContainer = document.querySelector(".collapse-toolbar");
 let x = document.querySelector("#tool-bar");
-// let inDiv = document.querySelectorAll(".msg");
+let exit = document.createElement('img');
+let footerDiv = document.createElement('div');
+let footerHr = document.createElement('hr');
 
 div.addEventListener("click", openMenu);
 
@@ -9,7 +11,26 @@ function openMenu() {
   menuContainer.classList.remove("collapse-toolbar");
   menuContainer.classList.add("menu-list");
   x.classList.add("hide-top");
-  // inDiv.forEach((element) => {
-  //   element.classList.add("ms");
-  // });
+
+  exit.src = "./images/exit.png";
+  exit.classList.add('exit-img');
+
+  footerDiv.classList.add('last-div');
+  footerHr.classList.add('footer-hr');
+  footerDiv.append(footerHr);
+  
+
+  menuContainer.prepend(exit);
+  menuContainer.append(footerDiv);
+}
+
+exit.addEventListener("click", closeMenu);
+
+function closeMenu() {
+  x.classList.remove("hide-top");
+  menuContainer.classList.add("collapse-toolbar");
+  menuContainer.classList.remove("menu-list");
+
+  footerDiv.removeChild(footerHr);
+  menuContainer.removeChild(footerDiv);
 }
