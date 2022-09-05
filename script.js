52e35ad3032b351885cf91c2896ea60e54c12768
Aminu Rabiu <arabiu033@gmail.com>
2022-09-05 18:22:03 +0100

    fix linter

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-let div = document.querySelector("#normal-buttons");
-let menuContainer = document.querySelector(".collapse-toolbar");
-let x = document.querySelector("#tool-bar");
-let exit = document.createElement('img');
-let footerDiv = document.createElement('div');
-let footerHr = document.createElement('hr');
+const div = document.querySelector('#normal-buttons');
+const menuContainer = document.querySelector('.collapse-toolbar');
+const x = document.querySelector('#tool-bar');
+const exit = document.createElement('img');
+const footerDiv = document.createElement('div');
+const footerHr = document.createElement('hr');
 
-div.addEventListener("click", openMenu);
+div.addEventListener('click', openMenu);
 
 function openMenu() {
-  menuContainer.classList.remove("collapse-toolbar");
-  menuContainer.classList.add("menu-list");
-  x.classList.add("hide-top");
+  menuContainer.classList.remove('collapse-toolbar');
+  menuContainer.classList.add('menu-list');
+  x.classList.add('hide-top');
 
-  exit.src = "./images/exit.png";
+  exit.src = './images/exit.png';
   exit.classList.add('exit-img');
 
   footerDiv.classList.add('last-div');
   footerHr.classList.add('footer-hr');
   footerDiv.append(footerHr);
   
-
   menuContainer.prepend(exit);
   menuContainer.append(footerDiv);
 }
 
-exit.addEventListener("click", closeMenu);
+exit.addEventListener('click', closeMenu);
 
 function closeMenu() {
-  x.classList.remove("hide-top");
-  menuContainer.classList.add("collapse-toolbar");
-  menuContainer.classList.remove("menu-list");
+  x.classList.remove('hide-top');
+  menuContainer.classList.add('collapse-toolbar');
+  menuContainer.classList.remove('menu-list');
 
   footerDiv.removeChild(footerHr);
   menuContainer.removeChild(footerDiv);
