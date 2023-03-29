a04b4493d376e3ffa96ce0cf08afffd41c8f8a5b
arabiu033 <arabiu033@gmail.com>
2023-03-29 16:15:32 +0100

    Refactor the portfolio

--- script.js
+++ script.js
@@ -1,81 +1,100 @@
 const projects = [
   {
-    urlM: './images/SnapshootPortfolio.png',
-    urlD: './desktop-images/SnapshootPortfolio.png',
-    title: 'Keeping track of hundreds of components',
-    workTitle: 'Multi-Post Stories<br />Gain+Glory',
-    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html'],
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`',
-    linklive: 'https://arabiu033.github.io/Portfolio-Project/',
-    linkSrc: 'https://github.com/arabiu033/Portfolio-Project',
+    urlM: './images/cherry.png',
+    urlD: './images/cherry.png',
+    title: 'Cherry Rooms',
+    workTitle: 'Cherry Rooms',
+    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html', 'RSwag', 'Jwt', 'React', 'Redux'],
+    description: 'This app provides comprehensive information about all laureates'
+    + 'and their wonderful nobel prize winning momments.Cherry Rooms is a front-end'
+    + 'React project designed for the purpose of booking rooms in hotels, motels, or'
+    + 'any other accommodation service. This project provides an intuitive and user-friendly'
+    + 'interface that allows users to easily browse and reserve rooms based on their'
+    + 'preferences and availability. The project is built using React, HTML, CSS, and JavaScript,'
+    + 'with the use of APIs to fetch data from the backend.ps',
+    linklive: 'https://cherryrooms.netlify.app/',
+    linkSrc: 'https://github.com/adel-gu/Cherry-Rooms-front-end',
   },
   {
-    urlM: './images/SnapshootPortfolio.png',
-    urlD: './desktop-images/SnapshootPortfolio.png',
-    title: 'Keeping track of hundreds of components',
-    workTitle: 'Multi-Post Stories<br />Gain+Glory',
-    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html'],
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`',
-    linklive: 'https://arabiu033.github.io/Portfolio-Project/',
-    linkSrc: 'https://github.com/arabiu033/Portfolio-Project',
+    urlM: './images/todolist.png',
+    urlD: './images/todolist.png',
+    title: 'To-do List',
+    workTitle: 'To-do List',
+    technologies: ['Css', 'JavScript', 'html', 'ES6', 'LocalStorage', 'Webaack'],
+    description: '"To-do list" is a tool that helps to organize your day. It simply lists the things that'
+    + 'you need to do and allows you to mark them as complete. Built using ES6 and Webpack',
+    linklive: 'https://arabiu033-todolist.netlify.app/',
+    linkSrc: 'https://github.com/arabiu033/to_do-list',
   },
   {
-    urlM: './images/SnapshootPortfolio.png',
-    urlD: './desktop-images/SnapshootPortfolio.png',
-    title: 'Keeping track of hundreds of components',
-    workTitle: 'Multi-Post Stories<br />Gain+Glory',
-    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html'],
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`',
-    linklive: 'https://arabiu033.github.io/Portfolio-Project/',
-    linkSrc: 'https://github.com/arabiu033/Portfolio-Project',
+    urlM: './images/podcats.png',
+    urlD: './images/podcats.png',
+    title: 'Podcast submit webpage',
+    workTitle: 'Podcast submit webpage',
+    technologies: ['Css', 'JavScript', 'html', 'semantics', 'boostrap'],
+    description: 'A simple pod conference landing page. my first capstone projet.',
+    linklive: 'https://podcast-capstone.netlify.app/',
+    linkSrc: 'https://github.com/arabiu033/pod_cast',
   },
   {
-    urlM: './images/SnapshootPortfolio.png',
-    urlD: './desktop-images/SnapshootPortfolio.png',
-    title: 'Keeping track of hundreds of components',
-    workTitle: 'Multi-Post Stories<br />Gain+Glory',
-    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html'],
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`',
-    linklive: 'https://arabiu033.github.io/Portfolio-Project/',
-    linkSrc: 'https://github.com/arabiu033/Portfolio-Project',
+    urlM: './images/pocolee.png',
+    urlD: './images/pocolee.png',
+    title: 'Pocolee - Budget app',
+    workTitle: 'Pocolee - Budget app',
+    technologies: ['Ruby', 'Rails', 'JavScript', 'html', 'Devise', 'Boostrap', 'Postgresql', 'TDD'],
+    description: 'This app provides comprehensive information about all laureates'
+    + 'and their wonderful nobel prize winning momments.Pocolee is a mobile web application'
+    + 'that can help you keep track of your daily financial transaction and manage your budget'
+    + 'where you have a list of transactions associated with a category so that you can see how much'
+    + 'money you spent and on what',
+    linklive: 'https://pocolee.onrender.com/',
+    linkSrc: 'https://github.com/arabiu033/budget_app',
   },
   {
-    urlM: './images/SnapshootPortfolio.png',
-    urlD: './desktop-images/SnapshootPortfolio.svg',
-    title: 'Keeping track of hundreds of components',
-    workTitle: 'Multi-Post Stories<br />Gain+Glory',
-    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html'],
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`',
-    linklive: 'https://arabiu033.github.io/Portfolio-Project/',
-    linkSrc: 'https://github.com/arabiu033/Portfolio-Project',
+    urlM: './images/space.png',
+    urlD: './images/space.png',
+    title: 'space-travelers-hub',
+    workTitle: 'space-travelers-hub',
+    technologies: ['React', 'Redux', 'Promises', 'Webpack', 'Rest Api', 'Boostrap', '', 'Jest'],
+    description: 'This is a web application for a company that provides commercial and scientific'
+    + 'space travel services. The application allows users to book rockets and join selected space missions.',
+    linklive: 'https://space-hubs.netlify.app/',
+    linkSrc: 'https://github.com/arabiu033/space_travelers_hub',
   },
   {
-    urlM: './images/SnapshootPortfolio.png',
-    urlD: './desktop-images/SnapshootPortfolio.svg',
-    title: 'Keeping track of hundreds of components',
-    workTitle: 'Multi-Post Stories<br />Gain+Glory',
-    technologies: ['Ruby on rails', 'Css', 'JavScript', 'html'],
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`',
-    linklive: 'https://arabiu033.github.io/Portfolio-Project/',
-    linkSrc: 'https://github.com/arabiu033/Portfolio-Project',
+    urlM: './images/nobel.png',
+    urlD: './images/nobel.png',
+    title: 'Nobel Laureates',
+    workTitle: 'Nobel Laureates',
+    technologies: ['React', 'Redux', 'Promises', 'Webpack', 'Rest Api', 'Boostrap', 'Jest'],
+    description: 'This app provides comprehensive information about all laureates and their wonderful nobel prize winning moments.',
+    linklive: 'https://nobels.netlify.app/',
+    linkSrc: 'https://github.com/arabiu033/capstone_project_0x03',
+  },
+  {
+    urlM: './images/country.png',
+    urlD: './images/country.png',
+    title: 'Countries Info',
+    workTitle: 'Countries Info',
+    technologies: ['React', 'Redux', 'Promises', 'Webpack', 'Rest Api', 'Boostrap', 'Jest'],
+    description: 'This is a simple API integration project displaying a list of countries'
+    + 'and its information fetching data using API The project is bundled using webpack and'
+    + 'makes use of dynamic code through Javascript. the project makes use of jest for unit testing.',
+    linklive: 'https://countrie-caps.netlify.app',
+    linkSrc: 'https://github.com/arabiu033/capstone_project_0x02',
   },
 ];
 
 // populating the work section with automatic generated cards
-const cardRow = document.querySelector('#card-row');
+const cardRow = document.querySelector('.card-row');
 for (let k = 0; k < projects.length; k += 1) {
   const card = document.createElement('div');
-  card.className = 'card';
+  const maincard = document.createElement('div');
+  maincard.className = 'card';
+  card.className = 'white-back';
 
-  const cardText = document.createElement('div');
-  cardText.className = 'card-text';
-  const projectTitle = document.createElement('h2');
+  maincard.style.backgroundImage = `url(${projects[k].urlM})`;
+  const projectTitle = document.createElement('h3');
   projectTitle.className = 'project-title';
   projectTitle.insertAdjacentHTML('beforeend', projects[k].workTitle);
   const tags1 = document.createElement('ul');
@@ -90,26 +109,16 @@ for (let k = 0; k < projects.length; k += 1) {
     tags1.append(li);
   }
 
-  const seeProj = document.createElement('div');
-  seeProj.className = 'see-project';
-  const btn = document.createElement('button');
-  btn.className = 'btn';
-  btn.setAttribute('type', 'button');
-  const btnText = document.createElement('span');
+  const btnText = document.createElement('p');
   btnText.className = 'btn-text';
-  btnText.textContent = 'see Project';
-  btn.append(btnText);
-  seeProj.append(btn);
+  btnText.textContent = 'See Project';
 
-  cardText.append(projectTitle);
-  cardText.append(tags1);
-  cardText.append(seeProj);
-  card.append(cardText);
+  card.append(projectTitle);
+  card.append(tags1);
+  card.append(btnText);
 
-  const imgHolder = document.createElement('div');
-  imgHolder.className = 'img-holder';
-  card.append(imgHolder);
-  cardRow.append(card);
+  maincard.append(card);
+  cardRow.append(maincard);
 }
 
 const recentWork = document.querySelector('#recent-work');
@@ -120,7 +129,7 @@ const exit = document.createElement('img');
 const footerDiv = document.createElement('div');
 const footerHr = document.createElement('hr');
 const links = menuContainer.querySelectorAll('a');
-const seeProject = document.querySelectorAll('.btn');
+const seeProject = document.querySelectorAll('.btn-text');
 const abtm = document.querySelector('#about-me');
 const form = document.querySelector('#sign');
 const small = form.querySelector('small');
@@ -156,47 +165,54 @@ links.forEach((element) => {
 });
 
 const popConstruction = (i) => `
-  <section class='work-info'>
-    <button type='btn' class='cancel-btn'>
-      <img src='./images/cancel.png' alt='icon'/>
-    </button>
-    <div class='work-image'>
-    <img class='Mobile-img' src=${projects[i].urlM} alt='icon'/>
-    </div>
-    <h1 class='work-title'>
-      ${projects[i].title}
-    </h1>
-    <ul class='tech-list'>
-      <li class='list1'>${projects[i].technologies[0]}</li>
-      <li class='list1'>${projects[i].technologies[1]}</li>
-      <li class='list1'>${projects[i].technologies[2]}</li>
-      <li class='list1'>${projects[i].technologies[3]}</li>
-      <li class='list1'>${projects[i].technologies[0]}</li>
-    </ul>
-    <p class='description'>
-      ${projects[i].description}
-    </p>
-    <div class='work-buttons'>
-      <a href=${projects[i].linklive} class='wrk-btn'>
-        See Live
-        <img src='./images/seeLive.png' alt='see live'/>
-      </a>
-      <a href=${projects[i].linkSrc} class='wrk-btn'>
-        See Source
-        <img src='./images/seeSrc.png' alt='see live'/>
-      </a>
-    </div>
-    <script src='script.js'></script>
-  </section>`;
+
+<div class="icon-image">
+<div class="back-cross">
+  <img class="cross-popup-icon" src="images/cancel.png" alt="" />
+</div>
+<div class="popup-image">
+  <img src=${projects[i].urlM} />
+</div>
+</div>
+<div class="title-and-tech">
+<div>
+  <h1 class="popup-title">${projects[i].title}</h1>
+</div>
+<div>
+  <ul class="popup-tech tech-list-${i}">
+  </ul>
+</div>
+</div>
+<div class="popup-description">${projects[i].description}</div>
+<div class="popup-buttons">
+<div class="live-button">
+  <a class="live" target="_blank" href=${projects[i].linklive}>
+    See Live
+  </a>
+  <img src="./images/seeLive.png" alt="" />
+</div>
+<div class="live-button ml">
+  <a class="live" target="_blank" href=${projects[i].linkSrc}>
+    See Source
+  </a>
+  <img src="./images/seeSrc.png" alt="" />
+</div>
+</div>
+<script src='script.js'></script>`;
 
 for (let i = 0; i < seeProject.length; i += 1) {
   seeProject[i].addEventListener('click', () => {
     const divv = document.createElement('div');
+    divv.className = 'work-info';
     divv.innerHTML = popConstruction(i);
     document.body.appendChild(divv);
+    const techContainer = document.querySelector(`.tech-list-${i}`);
+    projects[i].technologies.forEach((t) => {
+      techContainer.innerHTML += `<li class="popup-tech-li">${t}</li>`;
+    });
     recentWork.classList.add('blurr');
     abtm.classList.add('blurr');
-    const cancelBtn = document.querySelector('.cancel-btn');
+    const cancelBtn = document.querySelector('.cross-popup-icon');
     cancelBtn.addEventListener('click', async () => {
       document.body.removeChild(divv);
       abtm.classList.remove('blurr');
